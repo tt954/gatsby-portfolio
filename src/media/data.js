50b5543884ea7dce9776643033e47fb5a91041f4
Tieulam Thai <ttl.t954@gmail.com>
2021-04-28 13:59:33 -0600

    add: project data

---
+++ src/media/data.js
@@ -0,0 +1,18 @@
+export const projectData = [
+  {
+    title: "wavetape",
+    technologies: ["ruby on rails", "react", "redux", "aws", "postgresql"],
+    description: "a Soundcloud clone.",
+    ghlink: "#",
+    livelink: "#",
+    imgSrc: "#"
+  },
+  {
+    title: "match",
+    technologies: ["javascript"],
+    description: "matching card game",
+    ghlink: "#",
+    livelink: "#",
+    imgSrc: "#"
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,39 @@
 import React from "react"
-import Header from '../components/header'
+import Header from "../components/header"
+import Project from "../components/project"
 
-import '../styles/index.scss'
+import { projectData } from '../media/data'
+import "../styles/index.scss"
 
 export default function Home() {
+  const projects = projectData.map((project, idx) => (
+    <Project
+      key={idx}
+      title={project.title}
+      technologies={project.technologies}
+      description={project.description}
+      ghlink={project.ghlink}
+      livelink={project.livelink}
+      imgSrc={project.imgSrc}
+    />
+  ))
+
   return (
     <div>
       <Header />
-      <h1>Hello World!</h1>
-      <p>My name is Tieulam</p>
+      <div className="section__hero">
+        <h1>
+          Hello World{" "}
+          <span role="img" aria-label="world" style={{ fontStyle: "normal" }}>
+            🌎
+          </span>
+        </h1>
+        <p>My name is Tieulam.</p>
+      </div>
+
+      <div className="section__projects">
+        {projects}
+      </div>
     </div>
   )
 }
