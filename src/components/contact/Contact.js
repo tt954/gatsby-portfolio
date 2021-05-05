4980b413327302a96580e8adde6c1ce79565991a
Tieulam Thai <ttl.t954@gmail.com>
2021-05-05 15:37:56 -0600

    add: first and last name inputs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import TextField from "@material-ui/core/TextField"
-import Button from "@material-ui/core/Button"
+import React, { useState } from 'react'
+import { TextField, Button } from "@material-ui/core/"
 
 export default function Contact() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+
+    const handleFNChange = e => setFirstName(e.target.value)
+    const handleLNChange = e => setLastName(e.target.value)
+
     return (
       <div className="main__contact">
         <form className="contactForm">
-          <TextField required id="standard-basic" label="First Name" />
-          <TextField required id="standard-basic" label="Last Name" />
+          <TextField
+            required
+            id="standard-basic"
+            label="First Name"
+            value={firstName}
+            onChange={handleFNChange}
+          />
+          <TextField
+            required
+            id="standard-basic"
+            label="Last Name"
+            value={lastName}
+            onChange={handleLNChange}
+          />
           <TextField required id="standard-basic" label="Email" />
           <TextField
             id="outlined-multiline-static"
@@ -17,6 +34,7 @@ export default function Contact() {
             variant="outlined"
           />
           <Button color="secondary">Secondary</Button>
+          {console.log(firstName)}
         </form>
       </div>
     )
